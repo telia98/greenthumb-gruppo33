6bcee619c5e9ee209463384a2198067ec621bb73
teresaelia <teresaelia@mbp-di-teresa-2.home>
2020-06-21 00:04:40 +0200

    correzione homepage

--- greenthumb/src/greenthumbGeneral/Homepage.java
+++ greenthumb/src/greenthumbGeneral/Homepage.java
@@ -2,13 +2,13 @@ package greenthumbGeneral;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.Toolkit;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,62 +18,87 @@ public class Homepage extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel logoPanel;
+	private JPanel appNamePanel;
+	private JPanel welcomePanel;
 	private JPanel infoPanel;
 	private JPanel loginPanel;
 	
 	public Homepage() {
-		Font font = new Font("Comic sans", Font.BOLD, 17);
+		Font fontBig = new Font("Herculanum", Font.BOLD, 45);
+		Font fontMedium = new Font("Herculanum", Font.BOLD, 25);
+		Font font = new Font("Comic sans", Font.PLAIN, 13);
 		
 		logoPanel= new JPanel();
+		logoPanel.setBackground(Color.WHITE);
+		appNamePanel= new JPanel();
+		appNamePanel.setBackground(Color.WHITE);
+		welcomePanel= new JPanel();
+		welcomePanel.setBackground(Color.WHITE);
 		infoPanel= new JPanel();
+		infoPanel.setBackground(Color.WHITE);
 		loginPanel= new JPanel();
+		loginPanel.setBackground(Color.WHITE);
 		
 		//elementi logoPanel
 		JButton logo=new JButton();
-		logo.setContentAreaFilled(false); 
-		logo.setBorder(null); 
-		logo.setFocusPainted(false); //per non far uscire i bordi blu del bottone quando selezionato
-		logo.setIcon(new ImageIcon("img/logo.png"));
-		logo.setSize(100, 100);
-		logo.setVisible(true);
 		
-		JLabel appName=new JLabel("<html><center>GreenThumb</center></html>");
-		appName.setFont(font);
-		appName.setForeground(new Color(45,174,0));
-		appName.setVisible(true);
+		//qui puoi settare le dimensioni del tuo pulsante , la foto si adattera (Se però le proporzioni non sono giuste si deforma anche)
+		logo.setPreferredSize(new Dimension(250,250));
 		
-		logoPanel.setLayout(new GridLayout(2,3)); //2 righe, 3 colonne
+		try {
+			Image logoImg=ImageIO.read(new File("/Users/teresaelia/git/greenthumb-gruppo33/greenthumb/img/logo.png"));
+			logoImg=logoImg.getScaledInstance(80,80,Image.SCALE_SMOOTH);
+			ImageIcon icona=new ImageIcon(logoImg);
+			logo.setIcon(icona); 
+			logo.setBorder(null); 
+			logo.setFocusPainted(false); //per non far uscire i bordi blu del bottone quando selezionato
+			logo.setVisible(true);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		logoPanel.setLayout(new GridLayout(1,3)); //1 riga, 3 colonne
 		logoPanel.add(new JLabel(""));
 		logoPanel.add(logo);
 		logoPanel.add(new JLabel(""));
-		logoPanel.add(new JLabel(""));
-		logoPanel.add(appName);
-		logoPanel.add(new JLabel(""));
+		
+		//elementi appNamePanel
+		JLabel appName=new JLabel("<html><center>GreenThumb</center></html>");
+		appName.setFont(fontBig);
+		appName.setForeground(new Color(45,174,0));
+		appName.setVisible(true);
+		appNamePanel.add(appName);
+		
+		//elementi welcomePanel
+		JLabel welcome=new JLabel("<html><center> Benvenuto! <br></center><html>");
+		welcome.setFont(fontMedium);
+		welcome.setForeground(new Color(45,174,0)); //RGB colore verde del logo
+		welcomePanel.add(welcome);
 		
 		//elementi infoPanel
-		JLabel welcome=new JLabel("<html><center> Benvenuto! <br><br></center><html>");
-		welcome.setFont(font);
-		welcome.setForeground(new Color(45,174,0)); //RGB colore verde del logo
-		JLabel infoApp = new JLabel("<html><center>L'applicazione ti dà la possibilità di<br>gestire i tuoi impianti d'irrigazione e i relativi costi,<br>e verificare lo stato delle tue colture.<br><br>Effettua il login per cominciare!</center></html>");
-		infoPanel.add(welcome);
-		infoPanel.add(infoApp);
+		JLabel infoApp = new JLabel("<html><center>L'applicazione ti dà la possibilità di<br>gestire i tuoi impianti d'irrigazione,<br>monitorare le relative spese<br>e verificare lo stato delle tue colture.<br><br>Effettua il login per cominciare!<br><br><br><br></center></html>");
+		infoApp.setFont(font);
+		infoPanel.add(infoApp,BorderLayout.NORTH);
 		
 		//elementi loginPanel
 		JButton login=new JButton("Login");
-		login.setForeground(new Color(45,174,0));
-		login.setFont(font);
-		logo.setFocusPainted(false); 
-		login.setSize(300,500);
+        login.setForeground(new Color(45,174,0));
+		login.setFont(fontMedium);
+		login.setFocusPainted(false); 
 		login.setVisible(true);
-		loginPanel.add(login); //aggiungo il pannello a sud del frame
+		loginPanel.add(login);
 		
 		//setting Jpanel schermata homepage
 		logoPanel.setVisible(true);
+		appNamePanel.setVisible(true);
+		welcomePanel.setVisible(true);
 		infoPanel.setVisible(true);
 		loginPanel.setVisible(true);
 		
-		setLayout(new GridLayout(3,1));
+		setLayout(new GridLayout(5,1));
 		add(logoPanel);
+		add(appNamePanel);
+		add(welcomePanel);
 		add(infoPanel);
 		add(loginPanel);
 	}
