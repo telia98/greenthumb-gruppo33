56abd380476213c1cf130db6ea5943b89ba7bf1f
MaryCer <61543594+MaryCer@users.noreply.github.com>
2020-06-23 00:14:15 +0200

    Gestione terreno UPDATE

---
+++ greenthumb/src/gestioneterreno/ModalitaIrrigazione.java
@@ -0,0 +1,12 @@
+package gestioneterreno;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ModalitaIrrigazione extends JPanel {
+	
+	public ModalitaIrrigazione (JFrame frame, String impianto) {
+		
+	}
+
+}
